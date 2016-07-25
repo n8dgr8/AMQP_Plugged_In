c52d56b9dd78cd0264e95d0ad7c84bb859967979
n8dgr8 <nathan@kwintek.net>
2016-07-24 23:34:37 -0500

    I should sleep

--- amqp_plugged_in.rb
+++ amqp_plugged_in.rb
@@ -1,23 +1,57 @@
 require 'bunny'
+require 'json'
 
 class AMQP_Plugged_In
 
-  attr_accessor :mq_connection
-  attr_accessor :mq_channel
-  attr_accessor :mq_exchange
-  attr_accessor :mq_queue
+  attr_accessor :mq_connection_rx
+  attr_accessor :mq_channel_rx
+  attr_accessor :mq_exchange_rx
+  attr_accessor :mq_queue_rx
 
-  def connect(mq_connection_in)
-    mq_connection = mq_connection_in
-    mq_connection.start
-    mq_channel = mq_connection.create_channel
-    mq_exchange = mq_channel.topic('event')
-    mq_queue = mq_channel.queue("", :exclusive => true)
+  attr_accessor :mq_connection_tx
+  attr_accessor :mq_channel_tx
+  attr_accessor :mq_exchange_tx
+
+  def connect(mq_connection_in_rx)
+    mq_connection_rx = mq_connection_in_rx
+    mq_connection_rx.start
+    mq_channel_rx = mq_connection_rx.create_channel
+    mq_exchange_rx = mq_channel_rx.topic('event')
+    mq_queue_rx = mq_channel_rx.queue("", :exclusive => true)
+
+
   end
 
   def bind_queue(routing_key)
-    mq_queue.bind(mq_exchange, :routing_key => routing_key)
+    mq_queue.bind(mq_exchange_rx, :routing_key => routing_key)
   end
 
+  def wait_for_message
+    begin
+      mq_queue_rx.subscribe(:block => true) do |_, _, message_body|
+        handle_message(message_body)
+      end
+    rescue Interrupt => _
+      mq_channel.close
+      mq_connection.close
+    end
+  end
+
+  def handle_message(message_body_as_string)
+    message_hash = JSON.parse(message_body_as_string)
+    #   Step 2 - Put your message handler logic here!
+    #   Step 3 - Publish an action message (if needed)
+    publish_action_message(routing_key, JSON.generate({:hello => 'bye'}))
+  end
+
+  def publish_action_message(routing_key, message)
+    #  mq_connection_tx = Bunny.new
+    #  mq_connection_tx.start
+    mq_channel_tx = mq_connection_tx.create_channel
+    mq_exchange_tx = mq_channel_tx.default_exchange
+    mq_exchange_tx.publish(message, :headers => {'type' => 'action'}, :routing_key => routing_key)
+
+    #  mq_connection_tx.close
+  end
 
 end
--- spec/amqp_plugged_in_spec.rb
+++ spec/amqp_plugged_in_spec.rb
@@ -18,15 +18,35 @@ describe 'On start' do
     amqp_plugged_in_ut.connect(connection_mock)
   end
 
-  it 'AMQP_Plugged_In binds to the desired topic on the exchange' do
-    # pending('Step 1 - Bind The Queue To Your Desired Topic')
-    amqp_plugged_in_ut.mq_queue = queue_mock
-    amqp_plugged_in_ut.mq_exchange = exchange_mock
-
-    expect(queue_mock).to receive(:bind).with(exchange_mock, :routing_key => 'abc.def.*')
-
-    amqp_plugged_in_ut.bind_queue('abc.def.*')
+  it 'AMQP_Plugged_In subscribes and waits for a message' do
+    amqp_plugged_in_ut.mq_queue_rx = queue_mock
+    expect(queue_mock).to receive(:subscribe).with({:block => true})
+    amqp_plugged_in_ut.wait_for_message
   end
 
-  # it 'AMQP_PLugged_In'
+  it 'AMQP_Plugged_In binds to the desired topic on the exchange' do
+    pending('Step 1 - Bind The Queue To Your Desired Topic')
+    amqp_plugged_in_ut.mq_queue_rx = queue_mock
+    amqp_plugged_in_ut.mq_exchange_rx = exchange_mock
+
+    expect(queue_mock).to receive(:bind).with(exchange_mock, :routing_key => 'YOUR.ROUTING.KEY.HERE.*')
+
+    amqp_plugged_in_ut.bind_queue('barf')
+  end
+
+  it 'AMQP_Plugged_In publishes valid action messages' do
+    pending('Step 3 - Publish A Valid JSON Message To The Default Exchange')
+
+    amqp_plugged_in_ut.mq_connection_tx = connection_mock
+
+    allow(connection_mock).to receive(:create_channel).and_return(channel_mock)
+    allow(channel_mock).to receive(:default_exchange).and_return(exchange_mock)
+
+    expect(exchange_mock).to receive(:publish).with(
+        '{"hello":"bye"}', {:headers=>{'type' => 'action'}, :routing_key => 'i.am.a.routing.key.*'})
+
+    amqp_plugged_in_ut.publish_action_message('i.am.a.routing.key.*', JSON.generate({:hello => 'bye'}))
+
+    assert(false, true)
+  end
 end
